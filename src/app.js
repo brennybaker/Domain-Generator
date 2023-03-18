b93250ef793d9a6fb6c51efe51f073e2ef7e2c5a
BreAnna Baker <baker95@nsuok.edu>
2023-03-18 00:42:28 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function(generateDomain) {};
+
+let pronoun = ["the", "our", "your"];
+let adj = ["great", "big", "awesome"];
+let noun = ["jogger", "racoon", "puppy"];
+let ext = [".com", ".edu", ".org"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < ext.length; l++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}${ext[l]}`);
+      }
+    }
+  }
+}
